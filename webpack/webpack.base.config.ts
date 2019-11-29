d688e488a12e078181fe710e1bbd02194f108bdb
min <0s.dongmin@gmail.com>
2019-11-29 14:58:58 +0900

    update webpack.base.config

--- webpack/webpack.base.config.ts
+++ webpack/webpack.base.config.ts
@@ -1,62 +1,65 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const AutoprefixerPlugin = require('autoprefixer')
+module.exports = (() => {
+  const path = require('path')
+  const HtmlWebpackPlugin = require('html-webpack-plugin')
+  const AutoprefixerPlugin = require('autoprefixer')
 
-module.exports = {
-  entry: path.join(__dirname, '..', 'src', 'index.tsx'),
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-    modules: [path.join(__dirname, '..', 'src'), 'node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        exclude: '/node_modules/',
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+  return {
+    entry: path.join(__dirname, '..', 'src', 'index.tsx'),
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+      modules: [path.join(__dirname, '..', 'src'), 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
               },
             },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: [AutoprefixerPlugin()],
+          ],
+        },
+        {
+          test: /\.scss$/,
+          exclude: '/node_modules/',
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                },
+              },
             },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpg)$/,
-        use: ['file-loader'],
-      },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: [AutoprefixerPlugin()],
+              },
+            },
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        favicon: 'public/favicon.ico',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico',
-    }),
-  ],
-}
+  }
+})()
