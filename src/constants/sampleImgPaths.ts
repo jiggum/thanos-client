9cf91018b12ab44f78cfa17d38988b0af1bb5f40
min <0s.dongmin@gmail.com>
2019-12-24 23:50:28 +0900

    trim sample image input paths

--- src/constants/sampleImgPaths.ts
+++ src/constants/sampleImgPaths.ts
@@ -17,47 +17,47 @@ class SampleImgPaths {
 
 export default [
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample1-input.png',
+    'https://thanossnap.org/samples/sample1-input.jpg',
     'https://thanossnap.org/samples/sample1-background.png',
     'https://thanossnap.org/samples/sample1-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample2-input.png',
+    'https://thanossnap.org/samples/sample2-input.jpg',
     'https://thanossnap.org/samples/sample2-background.png',
     'https://thanossnap.org/samples/sample2-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample3-input.png',
+    'https://thanossnap.org/samples/sample3-input.jpg',
     'https://thanossnap.org/samples/sample3-background.png',
     'https://thanossnap.org/samples/sample3-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample4-input.png',
+    'https://thanossnap.org/samples/sample4-input.jpg',
     'https://thanossnap.org/samples/sample4-background.png',
     'https://thanossnap.org/samples/sample4-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample5-input.png',
+    'https://thanossnap.org/samples/sample5-input.jpg',
     'https://thanossnap.org/samples/sample5-background.png',
     'https://thanossnap.org/samples/sample5-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample6-input.png',
+    'https://thanossnap.org/samples/sample6-input.jpeg',
     'https://thanossnap.org/samples/sample6-background.png',
     'https://thanossnap.org/samples/sample6-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample7-input.png',
+    'https://thanossnap.org/samples/sample7-input.jpg',
     'https://thanossnap.org/samples/sample7-background.png',
     'https://thanossnap.org/samples/sample7-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample8-input.png',
+    'https://thanossnap.org/samples/sample8-input.jpg',
     'https://thanossnap.org/samples/sample8-background.png',
     'https://thanossnap.org/samples/sample8-persons1.png',
   ),
   new SampleImgPaths(
-    'https://thanossnap.org/samples/sample9-input.png',
+    'https://thanossnap.org/samples/sample9-input.jpg',
     'https://thanossnap.org/samples/sample9-background.png',
     'https://thanossnap.org/samples/sample9-persons1.png',
   ),
